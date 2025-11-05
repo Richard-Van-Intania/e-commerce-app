2ef36bf2863b7c8aa73140aaf9f7fdccf67677d3
Ittipat Pattum <nampu@finema.co>
2025-11-05 18:58:32 +0700

    add folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: "123",
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: "/api",
+    },
+  },
 });
