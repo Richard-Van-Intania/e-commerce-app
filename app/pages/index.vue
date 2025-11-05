aa7bc3414dfba575f6b32731ce834cf8ad51113a
Ittipat Pattum <nampu@finema.co>
2025-11-05 22:37:53 +0700

    save

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-sm font-normal">Hello world!</h1>
-  <h1 class="text-sm font-medium">Hello world!</h1>
+  <div class="h-[38px] bg-black flex items-center justify-between">
+    <div class="w-[120px]"></div>
+    <div class="text-sm text-white font-[Satoshi-Variable]">
+      <span>Sign up and get 20% off to your first order.</span>
+      <NuxtLink class="underline pl-2" to="/">Sign Up Now</NuxtLink>
+    </div>
+    <div class="w-[120px] bg-amber-50">X</div>
+  </div>
 </template>
 
-<style scoped>
-h1 {
-  font-family: "Satoshi-Variable";
-}
-</style>
+<style scoped></style>
