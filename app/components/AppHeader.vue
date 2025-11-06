40aff3a4665c6cbdc8adc8b1670349e062f29bc8
Ittipat Pattum <nampu@finema.co>
2025-11-07 00:50:38 +0700

    save

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -4,6 +4,28 @@
     <div class="h-24 flex items-center">
         <div class="w-20"></div>
         <div class="text-[32px] font-[integralcf-bold]">SHOP.CO</div>
+        <div class="w-12"></div>
+        <button type="button" class="cursor-pointer">
+            <span class="text-[16px] font-[Satoshi-Variable]">Shop</span>
+            <span class="w-1"></span>
+            <Icon name="ri:arrow-down-s-line" style="color: black" size="24px" class="align-middle" />
+        </button>
+        <div class="w-12"></div>
+        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">On Sale</NuxtLink>
+        <div class="w-12"></div>
+        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">New Arrivals</NuxtLink>
+        <div class="w-12"></div>
+        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">Brands</NuxtLink>
+        <div class="w-12"></div>
+        <div class="flex-1 bg-cyan-500">max</div>
+        <div class="w-12"></div>
+        <button type="button" class="cursor-pointer">
+            <Icon name="ri:shopping-cart-2-line" size="20px" class="align-middle" />
+        </button>
+        <div class="w-6"></div>
+        <button type="button" class="cursor-pointer">
+            <Icon name="ri:account-circle-line" size="20px" class="align-middle" />
+        </button>
         <div class="w-20"></div>
     </div>
 </template>
--- app/components/TopBanner.vue
+++ app/components/TopBanner.vue
@@ -3,21 +3,15 @@ function close() {}
 </script>
 
 <template>
-    <div
-        class="h-[38px] max-sm:h-14 bg-black flex items-center justify-between"
-    >
+    <div class="h-[38px] max-sm:h-14 bg-black flex items-center justify-between">
         <div class="w-[120px]"></div>
         <div class="text-sm text-white font-[Satoshi-Variable] text-center">
             <span>Sign up and get 20% off to your first order.</span>
             <NuxtLink class="underline pl-2" to="/">Sign Up Now</NuxtLink>
         </div>
         <div class="w-[120px]">
-            <button
-                type="button"
-                class="cursor-pointer max-sm:hidden"
-                v-on:click="close()"
-            >
-                <Icon name="ri:close-large-line" style="color: white" />
+            <button type="button" class="cursor-pointer max-sm:hidden" v-on:click="close()">
+                <Icon name="ri:close-large-line" style="color: white" class="align-middle" />
             </button>
         </div>
     </div>
