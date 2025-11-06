f207775f22def8dc645fb4097b36591b8bd6a45b
Ittipat Pattum <nampu@finema.co>
2025-11-06 23:48:13 +0700

    save

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h-24">appbar</div>
+    <div class="h-24 flex items-center">
+        <div class="w-20"></div>
+        <div class="text-[32px] font-[integralcf-bold]">SHOP.CO</div>
+        <div class="w-20"></div>
+    </div>
 </template>
 
 <style scoped></style>
