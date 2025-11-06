ea8eb24eac09bbe114e8605222d4746bf7f037c5
Ittipat Pattum <nampu@finema.co>
2025-11-06 23:14:02 +0700

    Update TopBanner.vue

--- app/components/TopBanner.vue
+++ app/components/TopBanner.vue
@@ -5,12 +5,16 @@ function close() {}
 <template>
     <div class="h-[38px] bg-black flex items-center justify-between">
         <div class="w-[120px]"></div>
-        <div class="text-sm text-white font-[Satoshi-Variable]">
+        <div class="text-sm text-white font-[Satoshi-Variable] text-center">
             <span>Sign up and get 20% off to your first order.</span>
             <NuxtLink class="underline pl-2" to="/">Sign Up Now</NuxtLink>
         </div>
         <div class="w-[120px]">
-            <button type="button" class="cursor-pointer" v-on:click="close()">
+            <button
+                type="button"
+                class="cursor-pointer max-sm:hidden"
+                v-on:click="close()"
+            >
                 <Icon name="ri:close-large-line" style="color: white" />
             </button>
         </div>
