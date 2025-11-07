764b0de4f9981c9039931e14f4593737826cdb17
Ittipat Pattum <nampu@finema.co>
2025-11-07 14:52:49 +0700

    save

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function test() {
+    console.log("holaa");
+}
+</script>
 
 <template>
     <div class="h-24 flex items-center">
         <div class="w-20"></div>
-        <div class="text-[32px] font-[integralcf-bold]">SHOP.CO</div>
+        <NuxtLink class="text-[32px] font-[integralcf-bold]" to="/">SHOP.CO</NuxtLink>
         <div class="w-12"></div>
         <button type="button" class="cursor-pointer">
             <span class="text-[16px] font-[Satoshi-Variable]">Shop</span>
@@ -11,21 +15,17 @@
             <Icon name="ri:arrow-down-s-line" style="color: black" size="24px" class="align-middle" />
         </button>
         <div class="w-12"></div>
-        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">On Sale</NuxtLink>
+        <ButtonsNavText label="On Sale" destination="/"></ButtonsNavText>
         <div class="w-12"></div>
-        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">New Arrivals</NuxtLink>
+        <ButtonsNavText label="New Arrivals" destination="/"></ButtonsNavText>
         <div class="w-12"></div>
-        <NuxtLink class="text-[16px] font-[Satoshi-Variable]" to="/">Brands</NuxtLink>
+        <ButtonsNavText label="Brands" destination="/"></ButtonsNavText>
         <div class="w-12"></div>
         <div class="flex-1 bg-cyan-500">max</div>
         <div class="w-12"></div>
-        <button type="button" class="cursor-pointer">
-            <Icon name="ri:shopping-cart-2-line" size="20px" class="align-middle" />
-        </button>
+        <ButtonsNavIcon label="ri:shopping-cart-2-line" v-on:handle-click="test()"></ButtonsNavIcon>
         <div class="w-6"></div>
-        <button type="button" class="cursor-pointer">
-            <Icon name="ri:account-circle-line" size="20px" class="align-middle" />
-        </button>
+        <ButtonsNavIcon label="ri:account-circle-line" v-on:handle-click="test()"></ButtonsNavIcon>
         <div class="w-20"></div>
     </div>
 </template>
