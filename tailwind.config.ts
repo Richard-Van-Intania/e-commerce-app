2862c9640a01ce9562938cbd099133ecd8762ac5
Ittipat Pattum <nampu@finema.co>
2025-11-07 19:30:31 +0700

    save

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        "light-gray": "#7FFFD4",
+      },
+    },
+  },
+  plugins: [],
+};
