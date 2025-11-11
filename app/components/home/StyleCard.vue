bc2724cdceea0f4c99d2ac9fb1203b6afb84b691
Ittipat Pattum <nampu@finema.co>
2025-11-11 10:17:17 +0700

    save

--- app/components/home/StyleCard.vue
+++ app/components/home/StyleCard.vue
@@ -4,7 +4,7 @@
   <div class="bg-search-grey rounded-3xl">
     <div>
       <div class="text-[48px] font-[integralcf-bold] text-center pt-12 pb-10">BROWSE BY DRESS STYLE</div>
-      <div class="flex flex-wrap px-10 pb-10 gap-5">
+      <div class="flex flex-wrap justify-center px-10 pb-10 gap-4">
         <HomeDressCard source="/dress/Frame 61.png" v-on:handle-click="onClick('sss')"></HomeDressCard>
         <HomeDressCard source="/dress/Frame 62.png" v-on:handle-click="onClick('sss')"></HomeDressCard>
         <HomeDressCard source="/dress/Frame 64.png" v-on:handle-click="onClick('sss')"></HomeDressCard>
